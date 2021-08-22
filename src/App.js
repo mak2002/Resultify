640031a9fa2d4baefdba1e133a70b6f6d31835ad
mak2002 <makbond0902@gmail.com>
2021-08-22 15:20:41 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Sidebar from './components/Sidebar'
+import MainPage from './components/MainPage'
+import {BrowserRouter as Router , Switch, Route} from 'react-router-dom';
+import React, { useState, useEffect } from 'react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sidebar />
+      <MainPage />
     </div>
   );
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,10 @@
+import {BrowserRouter as Router , Switch, Route} from 'react-router-dom';
+import React, { useState, useEffect } from 'react'
+
+export default function Sidebar() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
